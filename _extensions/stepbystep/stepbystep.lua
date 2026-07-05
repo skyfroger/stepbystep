4713e4e1c34d5f3cbb7a9116c4d612b4e21b16b4
skyfroger <skyfroger@gmail.com>
2026-07-05 19:50:16 +0300

    feat: mark of pagebypage completion on the last page

--- _extensions/stepbystep/stepbystep.lua
+++ _extensions/stepbystep/stepbystep.lua
@@ -96,6 +96,15 @@ function createPbP(div, options)
               <span x-show="!isPageLocked(current + 1)" style="font-weight: bold;">]]..l10n("next")..[[:</span>
               <span x-html="stepHeaders[current + 1]"></span>
             </button>
+            <svg x-show="current === total - 1" width="64px" height="64px" viewBox="0 0 64 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+                <path d="M29.818,4.108C31.168,3.329 32.832,3.329 34.182,4.108C38.854,6.806 49.803,13.127 54.746,15.981C56.293,16.874 57.246,18.525 57.246,20.311L57.246,43.689C57.246,45.475 56.293,47.126 54.746,48.019C49.912,50.81 39.334,56.917 34.5,59.708C32.953,60.601 31.047,60.601 29.5,59.708C24.666,56.917 14.088,50.81 9.254,48.019C7.707,47.126 6.754,45.475 6.754,43.689L6.754,20.311C6.754,18.525 7.707,16.874 9.254,15.981C14.197,13.127 25.146,6.806 29.818,4.108Z" style="fill:url(#_Radial1);"/>
+                <g transform="matrix(1,0,0,0.991525,-4.468313,-3.569753)">
+                    <path d="M22.337,34.47L34.336,48.654L51.3,23.683" style="fill:none;stroke:rgb(240,223,120);stroke-width:7.63px;"/>
+                </g>
+                <defs>
+                    <radialGradient id="_Radial1" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="matrix(-26.515901,37.226258,-37.226258,-26.515901,41.06088,18.313874)"><stop offset="0" style="stop-color:rgb(135,211,162);stop-opacity:1"/><stop offset="1" style="stop-color:rgb(135,181,123);stop-opacity:1"/></radialGradient>
+                </defs>
+            </svg>
           </div>
         </div>]]))
 
